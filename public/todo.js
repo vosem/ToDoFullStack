4fa0fb75f8ae47a035f491058862a9afe38427c4
vosem <trefoil2@ukr.net>
2017-07-05 10:31:38 +0300

    disabling add button, adding submit upon enter

--- public/todo.js
+++ public/todo.js
@@ -53,8 +53,30 @@ $(document).ready(function(){
         });
     };
     
+
+
     ToDo.prototype.addTask = function(){
         __self = this;
+
+        function insertTask(){
+    	  var newTask = __self.inputField.val();    // alternative - document.forms["submitting"].elements["inputField"].value
+          var index = __self.length + 1;
+          __self.model.push({text: newTask, status: 'active'});
+          __self.updateLocalStorage();
+          __self.list.append('<tr><td class="task" data-index="'+index+'">'+index+'</td><td class="task" data-index="'+index+'">'+newTask+'</td><td><input class="up" type="button" data-index="'+index+'" value="&#8593"/></td><td><input class="delete" type="button" data-index="'+index+'" value="x"/></td></tr>');
+          __self.length++;
+          $('#inputField').val("New Task");
+          $('#inputButton').attr("disabled", "disabled");
+        }
+
+        $('#inputField').on('keypress', function(e){
+    		e = e || window.event;
+    		if (e.keyCode == 13 && $('#inputField').val() !='') {
+    			insertTask();
+          }
+        }); 
+
+
         $('#inputButton').on('click', function(){
             var newTask = __self.inputField.val();    // alternative - document.forms["submitting"].elements["inputField"].value
             var index = __self.length + 1;
@@ -62,8 +84,8 @@ $(document).ready(function(){
             __self.updateLocalStorage();
             __self.list.append('<tr><td class="task" data-index="'+index+'">'+index+'</td><td class="task" data-index="'+index+'">'+newTask+'</td><td><input class="up" type="button" data-index="'+index+'" value="&#8593"/></td><td><input class="delete" type="button" data-index="'+index+'" value="x"/></td></tr>');
             __self.length++;
-            __self.inputField.val('');
-            $('#inputField').val('New Task');
+            $('#inputField').val("New Task");
+            $('#inputButton').attr("disabled", "disabled");
         });        
     };
     
